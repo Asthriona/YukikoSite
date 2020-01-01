6dde6acd5bd3506f97ea887df0381ab1ed33b320
Asthriona <im@asthriona.com>
2020-01-01 22:26:37 +0100

    Add Leaderboard

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://yukikobot:jhLCvLlsw8lUORU1@yukiko-pcvs8.mongodb.net/discordbot?retryWrites=true&w=majority',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+  });
+
+var userDataSchema = new mongoose.Schema({
+  did: String,
+  username: String,
+  serverID: String,
+  xp: Number,
+  level: Number,
+  message: Number,
+  avatarURL: String,
+}, {collection: 'users'});
+
+var UsersData = mongoose.model('Users', userDataSchema);
 
 /* GET home page. */
 var title = "Yukiko Bot"
@@ -13,7 +30,29 @@ router.get('/fr', function(req, res, next) {
 router.get('/wp-admin', function(req, res, next) {
   res.render('wp-admin', { title: title + "you got jebaited" });
 });
-router.get('/login', function(req, res, next) {
+router.get('/wp-login', function(req, res, next) {
   res.render('wp-admin', { title: title + "you got jebaited" });
 });
+router.get('/data', function(req, res, next) {
+  res.render('data', { title: title + "you got jebaited" });
+});
+
+
+router.get('/548167755589877783', function(req, res, next){
+  UsersData.find({
+    serverID: '548167755589877783'
+  }).sort([['xp', 'descending']])
+    .then(function(users){
+      res.render('data', {items: users, title: title + "'s Leaderboard"})
+    })
+  });
+
+  router.get('/612216766680268811', function(req, res, next){
+    UsersData.find({
+      serverID: '612216766680268811'
+    }).sort([['xp', 'descending']])
+      .then(function(users){
+        res.render('data', {items: users, title: title + "'s Leaderboard"})
+      })
+    });
 module.exports = router;
