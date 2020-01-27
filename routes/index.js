0a1ed010ce1717f1a9bbf397cbdf22b0928d8223
Asthriona <im@asthriona.com>
2020-01-27 02:28:47 +0100

    Update leaderboard to a querry

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://yukikobot:jhLCvLlsw8lUORU1@yukiko-pcvs8.mongodb.net/discordbot?retryWrites=true&w=majority',{
+var siteconfig = require('../siteconfig.json')
+  mongoose.connect(siteconfig.dbLink,{
   useNewUrlParser: true,
   useUnifiedTopology: true
   });
-
 var userDataSchema = new mongoose.Schema({
   did: String,
   username: String,
@@ -33,26 +33,12 @@ router.get('/wp-admin', function(req, res, next) {
 router.get('/wp-login', function(req, res, next) {
   res.render('wp-admin', { title: title + "you got jebaited" });
 });
-router.get('/data', function(req, res, next) {
-  res.render('data', { title: title + "you got jebaited" });
-});
-
-
-router.get('/548167755589877783', function(req, res, next){
-  UsersData.find({
-    serverID: '548167755589877783'
-  }).sort([['xp', 'descending']])
+//LeaderBoard
+router.get('/lb?:id', function(req, res, next){
+  UsersData.find({serverID: req.query.id})
+  .sort([['xp', 'descending']])
     .then(function(users){
       res.render('data', {items: users, title: title + "'s Leaderboard"})
     })
   });
-
-  router.get('/612216766680268811', function(req, res, next){
-    UsersData.find({
-      serverID: '612216766680268811'
-    }).sort([['xp', 'descending']])
-      .then(function(users){
-        res.render('data', {items: users, title: title + "'s Leaderboard"})
-      })
-    });
 module.exports = router;
