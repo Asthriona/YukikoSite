de7f400dde8f7030327ceff9070a8cb97c4bf870
Asthriona <im@asthriona.com>
2020-05-09 08:16:06 +0200

    formating and add stuff to database

--- model/DiscordSite.js
+++ model/DiscordSite.js
@@ -1,12 +1,15 @@
 var mongoose = require('mongoose');
 
 var SiteShchema = new mongoose.Schema({
-    did: {type: String, required: true},
-    username: {type: String, required: true},
-    avatar: {type: String, required: false},
-    email: {type: String, required: true},
-    guilds: {type: Array, required: true}
+    did: { type: String, required: true },
+    username: { type: String, required: true },
+    tag: { type: String, required: true },
+    avatar: { type: String, required: false },
+    email: { type: String, required: true },
+    guilds: { type: Array, required: true },
+    premium: { type: String, required: false },
+    local: { type: String, required: true },
+    flags: { type: String, required: true },
 });
 
 var Site = module.exports = mongoose.model('Site', SiteShchema);
-
