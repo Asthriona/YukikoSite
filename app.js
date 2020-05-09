4118dcb50c156a8082160465c8d75670a8969996
Asthriona <im@asthriona.com>
2020-05-09 08:15:43 +0200

    better formating and update x-powered

--- app.js
+++ app.js
@@ -31,13 +31,14 @@ app.use(session({
   },
   saveUninitialized: false,
   name: 'Yukiko_Yummy_cookie',
-  store: new MongoStore({ mongooseConnection:  mongoose.connection })
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('x-powered-by', 'Poggy!');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -50,11 +51,11 @@ app.use('/auth', authRouter);
 app.use('/dashboard', dashboardRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
