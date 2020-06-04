febf9e3563db723b5fbc9e57967bb18034b0539e
Heazher <heazher@Asthriona.com>
2020-06-04 06:34:18 +0100

    add premium status to db

--- model/DiscordSite.js
+++ model/DiscordSite.js
@@ -5,11 +5,12 @@ var SiteShchema = new mongoose.Schema({
     username: { type: String, required: true },
     tag: { type: String, required: true },
     avatar: { type: String, required: false },
-    email: { type: String, required: true },
+    email: { type: String, required: false },
     guilds: { type: Array, required: true },
     premium: { type: String, required: false },
     local: { type: String, required: true },
     flags: { type: String, required: true },
+    sitePremium: { type: String, required: false }
 });
 
 var Site = module.exports = mongoose.model('Site', SiteShchema);
