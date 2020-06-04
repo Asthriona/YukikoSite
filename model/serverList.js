64ecfb5833312ed4e49f5132ad08693d0ee73f89
Heazher <heazher@Asthriona.com>
2020-06-04 06:58:04 +0100

    Revert "add server list model"
    This reverts commit 5991cc058a4f094d0d36ada9902eb9f124ea46c6.

--- model/serverList.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-
-var serverListSchema = new mongoose.Schema({
-    sid: String,
-    sname: String,
-    sicon: String,
-    sowner: String
-})
-module.exports = mongoose.model("ServerList", serverListSchema);
